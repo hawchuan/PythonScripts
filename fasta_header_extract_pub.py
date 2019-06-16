3266bca4d513f7daa0e76a148626c2e39c0df10d
hawchuan <limhc@si.edu>
2019-06-16 15:08:05 -0400

    Add files via upload

---
+++ fasta_header_extract_pub.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Original name: fasta_header_extract.py
+Author: Haw Chuan Lim 
+Purpose: Extract headers (ie the name after the > symbol) of a multisequence fasta file
+"""
+
+fasta1= open('test.fasta', 'r')
+
+fasta1out = open('fasta1name.out', 'w')
+
+for line in fasta1:
+	if line.startswith('>'):
+		line = line.strip('\n')
+		name = line.split()[0]
+		name.replace(">","")
+
+		print name
+		fasta1out.write(name + "\n")
+		
+fasta1out.close()
